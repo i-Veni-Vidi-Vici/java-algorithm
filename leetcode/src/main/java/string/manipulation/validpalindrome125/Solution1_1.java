3956c51dd8c7dde954361adeac159cc64f9c15ef
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-01 19:32:07 +0900

    feat: add Solution 1_1

---
+++ leetcode/src/main/java/string/manipulation/validpalindrome125/Solution1_1.java
@@ -0,0 +1,30 @@
+package string.manipulation.validpalindrome125;
+
+public class Solution1_1 {
+    public static boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            char front = s.charAt(start);
+            while (!Character.isLetterOrDigit(front) && start < end) {
+                front = s.charAt(++start);
+            }
+
+            char back = s.charAt(end);
+            while (!Character.isLetterOrDigit(back) && start < end) {
+                back = s.charAt(--end);
+            }
+
+            if (Character.toLowerCase(front) != Character.toLowerCase(back)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+
+    }
+}
