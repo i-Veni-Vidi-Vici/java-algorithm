a130322943a74298df204db066d999353412cfc6
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-01 18:44:05 +0900

    fix: modify 0-9 exclude error

--- leetcode/src/main/java/string/manipulation/validpalindrome125/Main.java
+++ leetcode/src/main/java/string/manipulation/validpalindrome125/Main.java
@@ -2,7 +2,7 @@ package string.manipulation.validpalindrome125;
 
 public class Main {
     public static boolean isPalindrome(String s) {
-        String lowerCase = s.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        String lowerCase = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         int length = lowerCase.length();
 
         for (int index = 0; index < length/2 ; index++) {
