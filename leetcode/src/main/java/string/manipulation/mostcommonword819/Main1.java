e83fb2e04d8e2d4e2b48c4e94a7e980fe9aa8e5c
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-03 17:54:23 +0900

    feat: modify regex Main solution

---
+++ leetcode/src/main/java/string/manipulation/mostcommonword819/Main1.java
@@ -0,0 +1,31 @@
+package string.manipulation.mostcommonword819;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main1 {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String[] words = paragraph.replaceAll("[^a-zA-Z]+", " ").toLowerCase().split(" ");
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 1) + 1);
+        }
+
+        for (String s : banned) {
+            wordCount.remove(s);
+        }
+
+        int max = 0;
+        String value = "";
+        for (String s : wordCount.keySet()) {
+            if (max < wordCount.get(s)) {
+                max = wordCount.get(s);
+                value = s;
+            }
+        }
+
+        return value;
+    }
+    // 11~12ms, 정규식 최적화로 인해,  if 문 제거됨
+}
