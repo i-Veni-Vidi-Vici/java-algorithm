5f30606bb551e7a37b16e3bcddf85108f7416128
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-04 17:06:11 +0900

    feat: add solution2

---
+++ leetcode/src/main/java/string/manipulation/groupanagrams49/Solution2.kt
@@ -0,0 +1,17 @@
+package string.manipulation.groupanagrams49
+
+class Solution2 {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val results: MutableMap<String, MutableList<String>> = mutableMapOf()
+
+        for (s in strs) {
+            // 문자열 정렬
+            val key = s.toCharArray().sorted().joinToString("");
+            // 만약 기존에 없던 키라면 빈 리스트를 삽입
+            results.getOrPut(key) { mutableListOf() }
+            // 키에 해당하는 리스트에 추가
+            results[key]!!.add(s)
+        }
+        return ArrayList<List<String>>(results.values)
+    }
+}
