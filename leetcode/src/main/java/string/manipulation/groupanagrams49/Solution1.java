27473b8f944e1e05fe8b31eb706c956707a59004
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-04 17:02:04 +0900

    feat: add solution1

---
+++ leetcode/src/main/java/string/manipulation/groupanagrams49/Solution1.java
@@ -0,0 +1,30 @@
+package string.manipulation.groupanagrams49;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution1 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> results = new HashMap<>();
+
+        for (String s : strs) {
+            // 문자열을 문자 배열로 변환
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            // 문자 배열을 키로 하기 위해 다시 문자열로 변환
+            String key = String.valueOf(chars);
+
+            // 만약 기존에 없던 키라면 빈 리스트를 삽입
+            if (!results.containsKey(key))
+                results.put(key, new ArrayList<>());
+            // 키에 해당하는 리스트에 추가
+            results.get(key).add(s);
+        }
+        return new ArrayList<>(results.values());
+    }
+
+    // 6ms
+}
