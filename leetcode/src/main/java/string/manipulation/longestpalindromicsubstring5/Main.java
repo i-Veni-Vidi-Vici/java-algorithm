708d5e77dcc8a25e35996bbad736292af3b44fdc
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-05 17:45:26 +0900

    feat: create 5. longeset palindromic substring

---
+++ leetcode/src/main/java/string/manipulation/longestpalindromicsubstring5/Main.java
@@ -0,0 +1,41 @@
+package string.manipulation.longestpalindromicsubstring5;
+
+import java.util.Arrays;
+
+public class Main {
+    public String longestPalindrome(String s) {
+        String[] values = s.split("");
+        String result = "";
+        String tmpResult = "";
+        int length = 0;
+        int tmpLength = 0;
+
+        boolean isPalindrome = false;
+
+        for (int i = 0; i < values.length; i++) {
+            for (int j = i; j < values.length; j++) {
+                if (values[i].equals(values[j]) && length < (j - i + 1)) {
+                    tmpLength = length;
+                    length = j - i + 1;
+                    isPalindrome = true;
+                    tmpResult = result;
+                    result = String.join("", Arrays.copyOfRange(values, i, j + 1));
+
+                    for (int k = 0; k < (j - i) / 2; k++) {
+                        if (!values[i + k + 1].equals(values[j - k - 1])) {
+                            isPalindrome = false;
+                            length = tmpLength;
+                            result = tmpResult;
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return result;
+    }
+
+    // time out
+}
---
+++ leetcode/src/main/java/string/manipulation/longestpalindromicsubstring5/Test.java
@@ -0,0 +1,11 @@
+package string.manipulation.longestpalindromicsubstring5;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        String[] values = {"a", "b", "c"};
+        System.out.println(String.join("",Arrays.copyOfRange(values, 0, 1)));
+        ;
+    }
+}
