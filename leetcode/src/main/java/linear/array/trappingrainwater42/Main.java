a43a40cf1245e71f589fbb8158a53abf1b2570c5
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-07 16:45:22 +0900

    feat: modify Main solution

--- leetcode/src/main/java/linear/array/trappingrainwater42/Main.java
+++ leetcode/src/main/java/linear/array/trappingrainwater42/Main.java
@@ -7,18 +7,21 @@ public class Main {
         boolean isTrue = false;
         int maxCount =0;
         int sum = 0;
-        int sumCount = 1;
+        int sumCount = 0;
         int result =0;
+        boolean isSum = true;
         for (int i = 0; i < height.length; i++) {
-            if (max <= height[i] && sumCount !=0) {
+            if (max <= height[i] && isSum) {
                 if (isTrue) {
                     beforeMax = max;
                 }
                 max = height[i];
                 maxCount++;
 
+                isSum = false;
                 isTrue = true;
             } else if(maxCount >0){
+                isSum = true;
                 sum += height[i];
                 sumCount++;
             }
@@ -33,6 +36,8 @@ public class Main {
 
         return result;
     }
+
+    // fail
 
     public static void main(String[] args) {
         System.out.println(trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
