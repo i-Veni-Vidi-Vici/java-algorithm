e0293c6bcdce357a2f341d5cfe87ee5c06373b9e
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-08 17:26:23 +0900

    feat: add Main1_2 solution

---
+++ leetcode/src/main/java/linear/array/arraypartition561/Main1_2.java
@@ -0,0 +1,18 @@
+package linear.array.arraypartition561;
+
+import java.util.Arrays;
+
+public class Main1_2 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums); // 최악 O(n^2)
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+
+        return result;
+    }
+
+    // 12ms, min 메서드 제거
+}
