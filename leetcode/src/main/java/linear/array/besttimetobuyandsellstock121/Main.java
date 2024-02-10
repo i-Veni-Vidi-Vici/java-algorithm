d6756bd9f6fdcfacee0d628b4832396936b6b3e7
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-10 12:16:22 +0900

    feat: create 121. best time to buy and sell stock solution

---
+++ leetcode/src/main/java/linear/array/besttimetobuyandsellstock121/Main.java
@@ -0,0 +1,38 @@
+package linear.array.besttimetobuyandsellstock121;
+
+public class Main {
+    public int maxProfit(int[] prices) {
+        int start = 0;
+        int end = prices.length - 1;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int result = 0;
+
+        while (start <= end) {
+            if (min > prices[start]) {
+                min = prices[start];
+
+                while (start < end) {
+                    if (max < prices[end]) {
+                        max = prices[end];
+
+                        if (result < max - min) {
+                            result = max - min;
+                        }
+                    }
+
+                    end--;
+                }
+
+                max = Integer.MIN_VALUE;
+                end = prices.length - 1;
+            }
+
+            start++;
+        }
+
+        return result;
+    }
+
+    // 293ms
+}
