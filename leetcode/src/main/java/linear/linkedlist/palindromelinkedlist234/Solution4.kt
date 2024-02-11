cf913c29c34fcf4d9f5113f27a967322f0734be3
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-11 16:50:53 +0900

    feat: add Solution4

---
+++ leetcode/src/main/java/linear/linkedlist/palindromelinkedlist234/Solution4.kt
@@ -0,0 +1,24 @@
+package linear.linkedlist.palindromelinkedlist234
+
+import java.util.*
+
+class Solution4 {
+    fun isPalindrome(head: Main.ListNode?): Boolean {
+        val deque: Deque<Int> = LinkedList()
+        // 연결 리스트를 데크에 삽입
+        var node = head
+        while (node != null) {
+            deque.add(node.`val`)
+            node = node.next
+        }
+
+        // 데크가 모두 비거나(짝수 개일 때) 1개 이하(홀수 개일 때)가 될 때까지 비교
+        while (!deque.isEmpty() && deque.size > 1) {
+            // 데크의 양 끝을 추출해 팰린드롬 여부 확인 [!== 인지 확인 필요]
+            if (deque.pollFirst() != deque.pollLast()) {
+                return false
+            }
+        }
+        return true
+    }
+}
