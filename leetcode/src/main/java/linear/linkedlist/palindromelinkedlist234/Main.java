2e0b59c407b39fcf0c9d56a699ca045de12d6328
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-11 16:18:15 +0900

    feat: create 234. palindrome linked list solution

---
+++ leetcode/src/main/java/linear/linkedlist/palindromelinkedlist234/Main.java
@@ -0,0 +1,43 @@
+package linear.linkedlist.palindromelinkedlist234;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> result = new ArrayList<>();
+        ListNode node =head;
+
+        while (node != null) {
+            result.add(node.val);
+            node = head.next;
+        }
+
+        List<Integer> reversed = new ArrayList<>(result);
+        Collections.reverse(reversed);
+
+        return result.equals(reversed);
+    }
+
+    // Memory Limit
+
+}
