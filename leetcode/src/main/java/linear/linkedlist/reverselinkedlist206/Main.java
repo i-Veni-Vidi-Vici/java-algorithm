d923b55ff1f037cdff068156cd80f7173d72bf7a
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-13 15:46:57 +0900

    feat: create 208. reverse linked list solution

---
+++ leetcode/src/main/java/linear/linkedlist/reverselinkedlist206/Main.java
@@ -0,0 +1,36 @@
+package linear.linkedlist.reverselinkedlist206;
+
+public class Main {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode result = null;
+
+        while (head != null) {
+            ListNode current = result;
+            result = head;
+            head = head.next;
+            result.next = current;
+        }
+
+        return result;
+    }
+
+
+    // 0ms
+}
