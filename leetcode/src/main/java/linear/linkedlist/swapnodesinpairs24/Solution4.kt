db9a079d8d7654733594bd68ffe0242ce8dac56a
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-16 00:27:29 +0900

    feat: add Solution4

---
+++ leetcode/src/main/java/linear/linkedlist/swapnodesinpairs24/Solution4.kt
@@ -0,0 +1,18 @@
+package linear.linkedlist.swapnodesinpairs24
+
+class Solution4 {
+    fun swapPairs(head: Main.ListNode?): Main.ListNode? {
+        // 현재 노드와 다음 노드가 있으면 반복
+        if (head?.next != null) {
+            // 다음 노드 선언
+            val p = head.next
+            // 다음 다음 노드를 파라미터로 전달하고 스왑된 값을 리턴 받음
+            head.next = swapPairs(head.next.next)
+            // 다음 다음 노드는 현재 노드로 지정
+            p.next = head
+            // 다음 노드 리턴
+            return p
+        }
+        return head
+    }
+}
