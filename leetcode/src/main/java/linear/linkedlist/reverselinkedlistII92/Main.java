347a5f8790503a26a5a59acfd15c00c428841404
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-18 15:18:05 +0900

    feat: create 192. reverse linked list II solution

---
+++ leetcode/src/main/java/linear/linkedlist/reverselinkedlistII92/Main.java
@@ -0,0 +1,65 @@
+package linear.linkedlist.reverselinkedlistII92;
+
+import javax.swing.plaf.LabelUI;
+
+public class Main {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode nodes = head;
+        ListNode first = null;
+        ListNode end = head;
+        int count = 1;
+        ListNode reverse = null;
+
+        if (left == right) {
+            return head;
+        }
+
+        while (count <= right) {
+            if (count < left) {
+                first = nodes;
+                nodes = nodes.next;
+                end = nodes;
+                count++;
+                continue;
+            }
+            ListNode tmp = reverse;
+            reverse = nodes;
+            nodes = nodes.next;
+            reverse.next = tmp;
+            count++;
+        }
+
+        if (first != null) {
+            first.next = reverse;
+        }
+
+        if (end != null) {
+            end.next = nodes;
+        }
+
+        if (left == 1) {
+            return reverse;
+        }
+
+        return head;
+    }
+
+    // 0ms
+}
