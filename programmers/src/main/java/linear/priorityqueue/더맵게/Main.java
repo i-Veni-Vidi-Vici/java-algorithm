eec08551d7bd016ae16dd600cbfcfe2e7225a699
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-28 19:26:23 +0900

    refactor: Modify priority queue

--- programmers/src/main/java/linear/priorityqueue/더맵게/Main.java
+++ programmers/src/main/java/linear/priorityqueue/더맵게/Main.java
@@ -1,23 +1,11 @@
 package linear.priorityqueue.더맵게;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if (o1 > o2) {
-                    return 1;
-                } else if (o1 < o2) {
-                    return -1;
-                }
-
-                return 0;
-            }
-        });
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         for (int food : scoville) {
             queue.add(food);
