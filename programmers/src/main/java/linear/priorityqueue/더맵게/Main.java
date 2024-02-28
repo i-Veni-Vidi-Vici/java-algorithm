ed3905df8e192e7d599b2b2b5207bed8cd3c4173
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-28 19:16:39 +0900

    feat: create 더 맵게 solution

---
+++ programmers/src/main/java/linear/priorityqueue/더맵게/Main.java
@@ -0,0 +1,40 @@
+package linear.priorityqueue.더맵게;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 > o2) {
+                    return 1;
+                } else if (o1 < o2) {
+                    return -1;
+                }
+
+                return 0;
+            }
+        });
+
+        for (int food : scoville) {
+            queue.add(food);
+        }
+
+        while (K > queue.peek()) {
+            if (queue.size() == 1) {
+                return -1;
+            }
+
+            queue.add(queue.poll() + (queue.poll() * 2));
+            answer++;
+        }
+
+        return answer;
+    }
+    // 정확성: 83.9
+    // 효율성: 16.1
+    // 합계: 100.0 / 100.0
+}
