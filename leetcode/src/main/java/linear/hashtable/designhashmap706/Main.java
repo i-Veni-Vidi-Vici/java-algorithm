07bd45f8c232273f1e72e5877970f1bfe0bb225a
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-02-29 19:22:15 +0900

    refactor: modify logic

--- leetcode/src/main/java/linear/hashtable/designhashmap706/Main.java
+++ leetcode/src/main/java/linear/hashtable/designhashmap706/Main.java
@@ -1,28 +1,37 @@
 package linear.hashtable.designhashmap706;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     static class MyHashMap {
         int[] hash;
         public MyHashMap() {
             hash = new int[1000001];
-            Arrays.fill(hash,-1);
+//            Arrays.fill(hash,-1);
         }
 
 
         public void put(int key, int value) {
+            if (value == 0) {
+                hash[key] = -1;
+                return;
+            }
             hash[key] = value;
+
         }
 
         public int get(int key) {
+            if (hash[key] == -1) {
+                return 0;
+            }
+            if (hash[key] == 0) {
+                return -1;
+            }
             return hash[key];
         }
 
         public void remove(int key) {
-            hash[key] = -1;
+            hash[key] = 0;
         }
     }
 
@@ -31,6 +40,6 @@ public class Main {
         a.put(1,1);
     }
 
-    // 32ms
+    // 32ms -> 17ms
 
 }
