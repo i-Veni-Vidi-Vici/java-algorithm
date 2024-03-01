c09d17dedb794b53b75467b234bef2fc91f0d09a
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-01 14:45:20 +0900

    feat: add Solution3

---
+++ leetcode/src/main/java/linear/hashtable/jewelsandstones771/Solution3.kt
@@ -0,0 +1,16 @@
+package linear.hashtable.jewelsandstones771
+
+class Solution3 {
+    fun numJewelsInStones(J: String, S: String): Int {
+        var count = 0
+        val freqs: MutableSet<Char> = HashSet()
+
+        // 보석(J) 종류 저장
+        for (j in J.toCharArray())
+            freqs.add(j)
+        // 돌(S)이 보석(J)인 경우 +1
+        for (s in S.toCharArray())
+            if (freqs.contains(s)) count++
+        return count
+    }
+}
