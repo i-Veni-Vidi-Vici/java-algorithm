a74006828ca0aa6e4ba129db263130bc9b02b2ea
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-03 10:22:13 +0900

    feat: create 347. top k frequent elements

---
+++ leetcode/src/main/java/linear/hashtable/topkfrequentelements347/Main.java
@@ -0,0 +1,37 @@
+package linear.hashtable.topkfrequentelements347;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int size =k;
+        int[] result = new int[size];
+
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        while (k > 0) {
+            int max = 0;
+            int select =0;
+
+            for (Integer i : map.keySet()) {
+                if (max < map.get(i)) {
+                    max = map.get(i);
+                    select =i;
+                }
+            }
+
+            map.remove(select);
+            result[size - k--] = select;
+        }
+
+        return result;
+    }
+
+    // 48ms
+}
