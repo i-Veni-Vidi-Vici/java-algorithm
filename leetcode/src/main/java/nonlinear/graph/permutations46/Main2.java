ed43e7a7b2fe0467e41cb34d42fb60c8b5e64b47
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-09 23:11:56 +0900

    feat: add 46 solution test

---
+++ leetcode/src/main/java/nonlinear/graph/permutations46/Main2.java
@@ -0,0 +1,36 @@
+package nonlinear.graph.permutations46;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main2 {
+    public static void dfs(int[] nums, List<Integer> temp, List<List<Integer>> result, int index) {
+        if (temp.size() == nums.length) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (temp.isEmpty()) {
+                temp.add(nums[i]);
+            }
+
+            for (int j = i+1+index; j < nums.length; j++) {
+                temp.add(nums[j]);
+                dfs(nums, temp, result, index+1);
+            }
+        }
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        dfs(nums, new ArrayList<>(), result, 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        permute(new int[]{1, 2, 3});
+    }
+    // error
+}
