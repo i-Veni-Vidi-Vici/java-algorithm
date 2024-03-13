586c8eace47d726b46976fd0938f09c25b47bf24
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-13 16:20:47 +0900

    feat(332): add solution practice

---
+++ leetcode/src/main/java/nonlinear/graph/reconstructitinerary332/Main3.java
@@ -0,0 +1,30 @@
+package nonlinear.graph.reconstructitinerary332;
+
+import java.util.*;
+
+public class Main3 {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        List<String> result = new LinkedList<>();
+
+        Map<String, PriorityQueue<String>> map = new HashMap<>();
+        for (List<String> ticket : tickets) {
+            map.putIfAbsent(ticket.get(0), new PriorityQueue<>());
+            map.get(ticket.get(0)).add(ticket.get(1));
+        }
+        String start = "JFK";
+        Deque<String> stack = new ArrayDeque<>();
+        stack.add(start);
+
+        while (!stack.isEmpty()) {
+            while (map.containsKey(stack.peek()) && !map.get(stack.peek()).isEmpty()) {
+
+                stack.push(map.get(stack.peek()).poll());
+            }
+
+            result.add(0,stack.pop());
+        }
+
+        return result;
+    }
+    // 9 ms
+}
