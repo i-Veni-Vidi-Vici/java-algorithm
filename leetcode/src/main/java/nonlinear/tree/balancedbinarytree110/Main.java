827644cae190870498803327710aa4bd02494adf
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-25 21:37:08 +0900

    feat(tree): create 110. balaned binary tree solution

---
+++ leetcode/src/main/java/nonlinear/tree/balancedbinarytree110/Main.java
@@ -0,0 +1,52 @@
+package nonlinear.tree.balancedbinarytree110;
+
+public class Main {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public boolean dfs(TreeNode node, int leftCount, int rightCount) {
+
+        if (node.left != null) {
+            if (!dfs(node.left, leftCount++, rightCount)) {
+                return false;
+            }
+        }
+        if (node.right != null) {
+            if (!dfs(node.right, leftCount, rightCount++)) {
+                return false;
+            }
+        }
+
+        if(Math.abs(leftCount-rightCount)>1){
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return dfs(root, 0, 0);
+    }
+    //error
+}
