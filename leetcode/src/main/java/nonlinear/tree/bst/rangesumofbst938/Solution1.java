fdedc073113b823d96f49244a9fafc7273442552
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-03-30 12:06:04 +0900

    feat(938): add solution1

---
+++ leetcode/src/main/java/nonlinear/tree/bst/rangesumofbst938/Solution1.java
@@ -0,0 +1,49 @@
+package nonlinear.tree.bst.rangesumofbst938;
+
+public class Solution1 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int rangeSumBST(TreeNode root, int low, int high) {
+        // 예외 처리
+        if (root == null) {
+            return 0;
+        }
+
+        // 결과 변수
+        int result = 0;
+        // 현재 노드의 값이 low와 high 사이에 있다면 결과에 추가
+        if (low <= root.val && root.val <= high) {
+            result = root.val;
+        }
+        // 자식 노드 재귀 DFS 진행
+        result += rangeSumBST(root.left, low, high);
+        result += rangeSumBST(root.right, low, high);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        rangeSumBST(new TreeNode(10, new TreeNode(5, new TreeNode(3), new TreeNode(7)),
+                new TreeNode(15, null, new TreeNode(18))), 7, 15);
+    }
+
+    // 1ms
+}
