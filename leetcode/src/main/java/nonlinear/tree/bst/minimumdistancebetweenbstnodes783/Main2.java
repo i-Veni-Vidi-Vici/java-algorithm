655c7db6341dc93be94a6a077403a388ebd8bc70
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-01 13:54:32 +0900

    feat(783): add practice solution

---
+++ leetcode/src/main/java/nonlinear/tree/bst/minimumdistancebetweenbstnodes783/Main2.java
@@ -0,0 +1,40 @@
+package nonlinear.tree.bst.minimumdistancebetweenbstnodes783;
+
+public class Main2 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int min = Integer.MAX_VALUE;
+    int prv = Integer.MIN_VALUE + 100000;
+
+    public int minDiffInBST(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        minDiffInBST(root.left);
+        min = Math.min(root.val - prv, min);
+        prv = root.val;
+        minDiffInBST(root.right);
+
+        return min;
+    }
+
+    // 0ms
+}
