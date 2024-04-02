391b3aa3932269e1cfbeb5a1d3b9e5dfb8f1914d
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-02 20:57:08 +0900

    feat(938): add practice solution

---
+++ leetcode/src/main/java/nonlinear/tree/bst/rangesumofbst938/Main3.java
@@ -0,0 +1,43 @@
+package nonlinear.tree.bst.rangesumofbst938;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main3 {
+             public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        int sum =0;
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.poll();
+            if (node.val <= high && node.val >= low) {
+                sum += node.val;
+            }
+
+            if (node.right != null && node.val <= high) {
+                stack.push(node.right);
+            }
+            if (node.left != null && node.val >= low) {
+                stack.push(node.left);
+            }
+
+        }
+
+        return sum;
+    }
+
+    // 2ms
+}
