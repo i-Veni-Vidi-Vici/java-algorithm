d0d362a8385f79f4f33d6a9e8c2c91e11263a8c0
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-03 21:24:48 +0900

    feat(104): add practice solution2

---
+++ leetcode/src/main/java/nonlinear/tree/maximumdepthofbinarytree104/Main2.java
@@ -0,0 +1,28 @@
+package nonlinear.tree.maximumdepthofbinarytree104;
+
+public class Main2 {
+      public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return 1 + Math.max(left, right);
+    }
+
+    // 0ms
+}
