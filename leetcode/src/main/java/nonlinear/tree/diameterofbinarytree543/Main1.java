f345fe9c44a9840a18427ec50b87600590634d6f
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-04 20:59:26 +0900

    refactor(Main1): modify Main1

--- leetcode/src/main/java/nonlinear/tree/diameterofbinarytree543/Main1.java
+++ leetcode/src/main/java/nonlinear/tree/diameterofbinarytree543/Main1.java
@@ -2,7 +2,7 @@ package nonlinear.tree.diameterofbinarytree543;
 
 public class Main1 {
 
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -21,30 +21,38 @@ public class Main1 {
         }
     }
 
-    public int dfs(TreeNode node) {
+    public static int dfs(TreeNode node) {
         if (node == null) {
             return -1;
         }
         int left = 0;
         int right = 0;
 
-        left = diameterOfBinaryTree(node.left);
-        right = diameterOfBinaryTree(node.right);
+        left = dfs(node.left);
+        right = dfs(node.right);
 
         return 1 + Math.max(left, right);
     }
 
-    public int diameterOfBinaryTree(TreeNode root) {
+    public static int diameterOfBinaryTree(TreeNode root) {
         if (root == null) {
             return 0;
         }
         int left =0;
         int right = 0;
-
+        if (root.left != null) {
             left = dfs(root.left);
-
+            left++;
+        }
+        if (root.right != null) {
             right = dfs(root.right);
+            right++;
+        }
         return left + right;
+    }
+
+    public static void main(String[] args) {
+        diameterOfBinaryTree(new TreeNode(1, null, new TreeNode(2)));
     }
 
 // error
