51b294973cd3490fb707a24c218e42c9ceca045d
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-12 11:59:29 +0900

    feat(938): add practice soluion - dfs - 반복구조

---
+++ leetcode/src/main/java/nonlinear/tree/bst/rangesumofbst938/Main1_2.java
@@ -0,0 +1,52 @@
+package nonlinear.tree.bst.rangesumofbst938;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// DFS - 반복구조
+public class Main1_2 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        int sum = 0;
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+
+            if (node.val >= low && node.val <= high) {
+                sum += node.val;
+            }
+
+            if (node.val > low && node.left != null) {
+                stack.push(node.left);
+            }
+
+            if (node.val < high && node.right != null) {
+                stack.push(node.right);
+            }
+        }
+
+        return sum;
+    }
+
+    // 2ms
+}
