833933fcc057928b65a0f71b30fe3294d4ab6169
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-12 13:44:08 +0900

    feat(938): add practice solution4 - BFS 반복구조

---
+++ leetcode/src/main/java/nonlinear/tree/bst/rangesumofbst938/Main1_3.java
@@ -0,0 +1,46 @@
+package nonlinear.tree.bst.rangesumofbst938;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// BFS 반복구조
+public class Main1_3 {
+      public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int result = 0;
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+
+            if (node.val >= low && node.val <= high) {
+                result += node.val;
+            }
+
+            if (node.val > low && node.left != null) {
+                queue.add(node.left);
+            }
+
+            if (node.val < high && node.right != null) {
+                queue.add(node.right);
+            }
+        }
+
+        return result;
+    }
+
+    // 2ms
+}
