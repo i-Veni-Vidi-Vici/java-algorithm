9042dde8cff7679520e3d09bb1b7b4b7c67d5385
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-22 20:49:21 +0900

    feat(787): modify practice2

--- leetcode/src/main/java/nonlinear/shortestpath/_45_cheapest_flights_within_k_stops_787/Practice2.java
+++ leetcode/src/main/java/nonlinear/shortestpath/_45_cheapest_flights_within_k_stops_787/Practice2.java
@@ -10,16 +10,17 @@ public class Practice2 {
             data.get(flight[0]).put(flight[1], Arrays.asList(flight[2], 0));
         }
 
-        PriorityQueue<Map.Entry<Integer, List<Integer>>> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.getValue().get(0)));
+        PriorityQueue<Map.Entry<Integer, List<Integer>>> pq = new PriorityQueue<>(
+                Comparator.comparingInt(o -> o.getValue().get(0)));
         pq.add(new AbstractMap.SimpleEntry<>(src, Arrays.asList(0, -1)));
 
         Map<Integer, List<Integer>> dist = new HashMap<>();
 
-
         while (!pq.isEmpty()) {
             Map.Entry<Integer, List<Integer>> polled = pq.poll();
-            int key = polled.getKey();
+            Integer key = polled.getKey();
             List<Integer> value = polled.getValue();
+
 
             if (!dist.containsKey(key) && value.get(1) <= k) {
                 if (key == dst) {
@@ -27,14 +28,15 @@ public class Practice2 {
                 }
 
                 dist.put(key, value);
-
-                for (Map.Entry<Integer, List<Integer>> entry : data.get(key).entrySet()) {
-                    if (value.get(1) < k) {
-                        pq.add(new AbstractMap.SimpleEntry<>(entry.getKey(),
-                                Arrays.asList(entry.getValue().get(0) + value.get(0),
-                                       value.get(1) + 1)));
+                if (data.containsKey(key)) {
+                    for (Map.Entry<Integer, List<Integer>> entry : data.get(key).entrySet()) {
+                        if (value.get(1) < k) {
+                            pq.add(new AbstractMap.SimpleEntry<>(entry.getKey(), Arrays.asList(entry.getValue().get(0) + value.get(0),
+                                            value.get(1) + 1)));
+                        }
                     }
                 }
+
             }
         }
 
@@ -42,8 +44,8 @@ public class Practice2 {
     }
 
     public static void main(String[] args) {
-        // [[4,1,1],[1,2,3],[0,3,2],[0,4,10],[3,1,1],[1,4,3]]
-        findCheapestPrice(5,new int[][]{{4,1,1},{1,2,3},{0,3,2}, {0,4,10}, {3,1,1}, {1,4,3}}, 2,1,0);
+        // [[0,1,1],[0,2,5],[1,2,1],[2,3,1]]
+        findCheapestPrice(4, new int[][]{{0,1,1}, {0,2,5}, {1,2,1}, {2,3,1}}, 0, 3, 1);
     }
     // error
 }
