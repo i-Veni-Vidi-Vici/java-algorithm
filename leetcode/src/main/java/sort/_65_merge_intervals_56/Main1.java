4d9e8b106c18bc1707f2b7c68cda1c4898383bc6
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-27 08:03:00 +0900

    fix(148): fix logic error

--- leetcode/src/main/java/sort/_65_merge_intervals_56/Main1.java
+++ leetcode/src/main/java/sort/_65_merge_intervals_56/Main1.java
@@ -14,12 +14,10 @@ public class Main1 {
         }
         List<List<Integer>> result = new ArrayList<>();
 
-
-
         int j=0;
         for (int i = 0; i < intervals.length-1; i++) {
             if (intervals[i][1] >= intervals[i + 1][0]) {
-                result.add(Arrays.asList(intervals[i][0], intervals[i + 1][1]));
+                result.add(Arrays.asList(intervals[i][0], Math.max(intervals[i + 1][1],intervals[i][1])));
                 j++;
             } else {
                 result.add(Arrays.asList(intervals[j][0], intervals[j][1]));
