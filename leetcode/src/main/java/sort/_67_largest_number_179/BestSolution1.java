22eb0f3460bd925826939b2349d0ec1ef96deee8
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-29 11:05:22 +0900

    feat(179): add BestSolution1

---
+++ leetcode/src/main/java/sort/_67_largest_number_179/BestSolution1.java
@@ -0,0 +1,43 @@
+package sort._67_largest_number_179;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BestSolution1 {
+    public String largestNumber(int[] nums) {
+        int n=nums.length;
+        if(n==0)
+        {
+            return "";
+        }
+        String str[]=new String[n];
+        for(int i=0;i<n;i++)
+        {
+            str[i]=Integer.toString(nums[i]);
+        }
+
+        Arrays.sort(str,new Comparator<String>(){
+            public int compare(String a,String b)
+            {
+                String str1=a+b;
+                String str2=b+a;
+                return str2.compareTo(str1);
+            }
+        });
+
+        if(str[0].equals("0"))
+        {
+            return "0";
+        }
+
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<n;i++)
+        {
+            sb.append(str[i]);
+        }
+
+        return sb.toString();
+    }
+
+    // 5ms
+}
