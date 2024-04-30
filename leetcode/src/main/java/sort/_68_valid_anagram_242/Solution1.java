784103a9147e1e560fc07a6e2bcb51c3d3671610
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-30 09:53:11 +0900

    feat(242): add solution1

---
+++ leetcode/src/main/java/sort/_68_valid_anagram_242/Solution1.java
@@ -0,0 +1,21 @@
+package sort._68_valid_anagram_242;
+
+import java.util.Arrays;
+
+public class Solution1 {
+    public String sort(String s) {
+        // 문자열을 문자 배열로 변환
+        char[] chars = s.toCharArray();
+        // 문자 배열을 정렬
+        Arrays.sort(chars);
+        // 문자 배열을 문자열로 만들어 리턴
+        return new String(chars);
+    }
+
+    public boolean isAnagram(String s, String t) {
+        // s를 정렬한 결과와 t를 정렬한 결과가 일치하는지 비교
+        return sort(s).equals(sort(t));
+    }
+
+    // 3ms
+}
