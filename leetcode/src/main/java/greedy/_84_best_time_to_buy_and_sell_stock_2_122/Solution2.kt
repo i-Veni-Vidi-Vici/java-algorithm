d75680a4ddecf773f11b373ffaed6ba5ed7fa11a
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-15 10:11:07 +0900

    faet(122): add Solution2

---
+++ leetcode/src/main/java/greedy/_84_best_time_to_buy_and_sell_stock_2_122/Solution2.kt
@@ -0,0 +1,14 @@
+package greedy._84_best_time_to_buy_and_sell_stock_2_122
+
+class Solution2 {
+    fun maxProfit(prices: IntArray): Int {
+        var result = 0
+        // 전체를 순회하며 다음번 값이 오르는 경우를 찾는다.
+        for (i in prices.indices - 1) {
+            // 다음번 값이 오르는 경우 항상 이익을 취한다.
+            if (prices[i + 1] > prices[i])
+                result += prices[i + 1] - prices[i]
+        }
+        return result
+    }
+}
