67899e0b8076d4ea5a557da776a4f6c4f422a1f6
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-26 11:23:40 +0900

    comment(169): write TODO

--- leetcode/src/main/java/divideandconquer/_89_majority_element_169/Solution3.java
+++ leetcode/src/main/java/divideandconquer/_89_majority_element_169/Solution3.java
@@ -2,6 +2,7 @@ package divideandconquer._89_majority_element_169;
 
 import java.util.Arrays;
 
+// TODO : 매우 간단한 방식
 public class Solution3 {
     public int majorityElement(int[] nums) {
         // 정렬 후 가운데 지점을 정답으로 리턴
