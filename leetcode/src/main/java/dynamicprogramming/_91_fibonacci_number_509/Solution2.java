99d198420254101a725c442a57369ea8ff23d897
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-27 16:46:17 +0900

    comment(509): write TODO

--- leetcode/src/main/java/dynamicprogramming/_91_fibonacci_number_509/Solution2.java
+++ leetcode/src/main/java/dynamicprogramming/_91_fibonacci_number_509/Solution2.java
@@ -1,2 +1,20 @@
-package dynamicprogramming._91_fibonacci_number_509;public class Solution2 {
+package dynamicprogramming._91_fibonacci_number_509;
+
+// TODO : 메모제이션 풀이
+public class Solution2 {
+    // 문제 제약 조건에 따라 30에서 1칸 더 크게 지정
+    int[] dp = new int[31];
+
+    public int fib(int n) {
+        // 0과 1인 상향식과 마찬가지로 값을 미리 지정
+        if (n <= 1)
+            return n;
+
+        // 미리 계산한 결과가 있다면 해당 값 리턴
+        if (dp[n] != 0)
+            return dp[n];
+        dp[n] = fib(n - 1) + fib(n - 2);
+        return dp[n];
+    }
+    // 0ms
 }
