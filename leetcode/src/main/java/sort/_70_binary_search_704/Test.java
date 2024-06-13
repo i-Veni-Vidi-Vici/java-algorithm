9bc4fc001736a632bbddc9a61cf418fc0a4b5976
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-06-13 09:14:22 +0900

    feat(704): create / & shift 연산자 test

---
+++ leetcode/src/main/java/sort/_70_binary_search_704/Test.java
@@ -0,0 +1,14 @@
+package sort._70_binary_search_704;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = Integer.MAX_VALUE + Integer.MAX_VALUE;
+        int b = (Integer.MAX_VALUE + Integer.MAX_VALUE) >>> 1;
+        int c = (Integer.MAX_VALUE + Integer.MAX_VALUE) / 2;
+        int d = (Integer.MAX_VALUE + Integer.MAX_VALUE) >> 1;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
