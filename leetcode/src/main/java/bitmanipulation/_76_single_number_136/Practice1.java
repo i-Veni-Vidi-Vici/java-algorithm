5e432a9b6e6948e212c1f6f746d9ef5b5979bfd1
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-06-18 13:53:54 +0900

    feat(136): create Practice1

---
+++ leetcode/src/main/java/bitmanipulation/_76_single_number_136/Practice1.java
@@ -0,0 +1,12 @@
+package bitmanipulation._76_single_number_136;
+
+public class Practice1 {
+    public int singleNumber(int[] nums) {
+        int result =0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+    // 1ms
+}
