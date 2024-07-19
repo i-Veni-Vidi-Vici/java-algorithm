c6a5616a5107737742abc03fca27d37fa0578a06
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-07-19 11:21:26 +0900

    faet(238): create Practice1

---
+++ leetcode/src/main/java/linear/array/productofarrayexceptself238/Practice1.java
@@ -0,0 +1,37 @@
+package linear.array.productofarrayexceptself238;
+
+import java.util.Arrays;
+
+public class Practice1 {
+    public int[] productExceptSelf(int[] nums) {
+        int result =1;
+        int zeroCount =0;
+        for (int num : nums) {
+            if (num == 0) {
+                zeroCount++;
+                continue;
+            }
+
+            result *= num;
+        }
+        if (zeroCount == 0) {
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = result / nums[i];
+            }
+        } else if (zeroCount==1){
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    nums[i] = 0;
+                }else {
+                    nums[i] = result;
+                }
+            }
+        }else {
+                Arrays.fill(nums,0);
+        }
+
+        return nums;
+    }
+
+    // 2ms
+}
