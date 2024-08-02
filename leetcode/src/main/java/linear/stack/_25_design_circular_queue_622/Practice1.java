c4203557b1cf6fff2886a95ab391de7268e33075
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-02 13:29:13 +0900

    feat(622): create Practice1

---
+++ leetcode/src/main/java/linear/stack/_25_design_circular_queue_622/Practice1.java
@@ -0,0 +1,86 @@
+package linear.stack._25_design_circular_queue_622;
+
+public class Practice1 {
+
+    class MyCircularQueue {
+
+        int[] value;
+        int front;
+        int rear;
+        int size;
+        int length;
+
+        public MyCircularQueue(int k) {
+            value = new int[k];
+            front = 0;
+            rear = 0;
+            size = k;
+            length = 0;
+        }
+
+        public boolean enQueue(int value) {
+            if (!isFull()) {
+                if (isEmpty()) {
+                    this.value[rear] = value;
+                    length++;
+                    return true;
+                }
+                rear = (rear + 1) % size;
+                this.value[rear] = value;
+                length++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int Front() {
+            if (!isEmpty()) {
+                return value[front];
+            }
+
+            return -1;
+        }
+
+        public int Rear() {
+            if (!isEmpty()) {
+                return value[rear];
+            }
+
+            return -1;
+        }
+
+        public boolean isEmpty() {
+            if (length == 0) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public boolean isFull() {
+            if (length == size) {
+                return true;
+
+            }
+            return false;
+        }
+
+        public boolean deQueue() {
+            if (!isEmpty()) {
+                value[front] = -1;
+                front = (front + 1) % size;
+                length--;
+                return true;
+            }
+
+            return false;
+        }
+
+
+    }
+
+    // Wrong Answer
+    //39 / 59 testcases passed
+
+}
