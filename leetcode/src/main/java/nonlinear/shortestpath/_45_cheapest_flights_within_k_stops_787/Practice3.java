865caa91b8552660d684738cdb729ffc19fba0a9
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-22 12:17:39 +0900

    feat(787): modify Practice3

--- leetcode/src/main/java/nonlinear/shortestpath/_45_cheapest_flights_within_k_stops_787/Practice3.java
+++ leetcode/src/main/java/nonlinear/shortestpath/_45_cheapest_flights_within_k_stops_787/Practice3.java
@@ -9,10 +9,10 @@ import java.util.PriorityQueue;
 
 public class Practice3 {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, Map<Integer,Integer>> map = new HashMap<>();
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
         for (int[] flight : flights) {
             map.putIfAbsent(flight[0], new HashMap<>());
-            map.get(flight[0]).put(flight[1],flight[2]);
+            map.get(flight[0]).put(flight[1], flight[2]);
         }
 
         PriorityQueue<List<Integer>> pq = new PriorityQueue<>((o1, o2) -> o2.get(2) - o1.get(1));
@@ -24,7 +24,11 @@ public class Practice3 {
             if (map.containsKey(value.get(0))) {
                 for (Integer key : map.get(value.get(0)).keySet()) {
                     if (value.get(2) + 1 < k) {
-                        pq.add(List.of(key, map.get(value.get(0)).get(key)+ value.get(1), value.get(2) + 1));
+
+                        if (key == dst) {
+                            return value.get(1) + map.get(value.get(0)).get(key);
+                        }
+                        pq.add(List.of(key, map.get(value.get(0)).get(key) + value.get(1), value.get(2) + 1));
                     }
                 }
             }
