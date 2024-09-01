4301bf64cbf2f8efe84be440f039bf87dce6cf93
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-09-01 13:14:17 +0900

    test(108): debug Pratice2

--- leetcode/src/main/java/nonlinear/tree/bst/_55_convert_sorted_array_to_binary_search_tree_108/Practice2.java
+++ leetcode/src/main/java/nonlinear/tree/bst/_55_convert_sorted_array_to_binary_search_tree_108/Practice2.java
@@ -1,7 +1,7 @@
 package nonlinear.tree.bst._55_convert_sorted_array_to_binary_search_tree_108;
 
 public class Practice2 {
-      public class TreeNode {
+      public static class TreeNode {
       int val;
       TreeNode left;
       TreeNode right;
@@ -14,7 +14,7 @@ public class Practice2 {
       }
   }
 
-    public TreeNode sortedArrayToBST(int[] nums) {
+    public static TreeNode sortedArrayToBST(int[] nums) {
         TreeNode root = new TreeNode();
 
         dfs(root, nums, (nums.length - 1) / 2);
@@ -22,7 +22,7 @@ public class Practice2 {
         return root;
     }
 
-    private TreeNode dfs(TreeNode node, int[] nums, int mid) {
+    private static TreeNode dfs(TreeNode node, int[] nums, int mid) {
         if (mid == 0 || mid == nums.length - 1) {
             return null;
         }
@@ -32,6 +32,11 @@ public class Practice2 {
         dfs(node.right, nums, (nums.length - 1 - (mid + 1)) / 2 + mid + 1);
 
         return node;
+    }
+
+    public static void main(String[] args) {
+
+        sortedArrayToBST(new int[]{-10,-3,0,5,9});
     }
     // error
 }
