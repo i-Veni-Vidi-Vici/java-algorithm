2262faa4eb256c3f90e19922daf670aa542be72b
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-09-06 13:45:50 +0900

    feat(215): create Practice3

---
+++ leetcode/src/main/java/nonlinear/heap/_60_Kth_Largest_Element_in_an_Array_215/Practice3.java
@@ -0,0 +1,23 @@
+package nonlinear.heap._60_Kth_Largest_Element_in_an_Array_215;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Practice3 {
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> queue = new PriorityQueue<>((o1, o2)-> o2 - o1);
+
+        for(int num : nums){
+            queue.add(num);
+        }
+
+        int result =0;
+        for(int i=0; i<k; i++){
+            result= queue.poll();
+        }
+
+        return  result;
+    }
+
+    // 61ms
+}
