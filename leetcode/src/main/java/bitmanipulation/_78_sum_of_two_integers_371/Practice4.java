c45dbc3bce60c8fe9bbad622272f390c457e3c0c
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-09-22 22:29:55 +0900

    feat(371): create Practice4

---
+++ leetcode/src/main/java/bitmanipulation/_78_sum_of_two_integers_371/Practice4.java
@@ -0,0 +1,32 @@
+package bitmanipulation._78_sum_of_two_integers_371;
+
+public class Practice4 {
+    public static int getSum(int a, int b) {
+        char[] valueA = String.format("%32s", Integer.toBinaryString(a)).replace(' ', '0').toCharArray();
+        char[] valueB = String.format("%32s", Integer.toBinaryString(b)).replace(' ', '0').toCharArray();
+
+        int carry =0;
+        StringBuilder sb = new StringBuilder();
+        for(int i=31; i>=0; i--){
+            int A = Character.getNumericValue(valueA[i]);
+            int B = Character.getNumericValue(valueB[i]);
+            int xor = A ^ B;
+
+            if((xor ^ carry) == 1){
+                sb.append(1);
+            } else sb.append(0);
+            if(((A&B)| (xor& carry)) == 1 ){
+                carry =1;
+            }else carry =0;
+        }
+
+        sb.reverse();
+
+        return Integer.parseInt(sb.toString(), 2);
+    }
+
+    public static void main(String[] args) {
+        getSum(1, 2);
+    }
+    // error
+}
