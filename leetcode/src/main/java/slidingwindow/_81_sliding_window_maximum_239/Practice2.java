d42ea75473460fc5fa1be27db3b59d5a3d5764e3
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-09-23 23:59:29 +0900

    feat(239): modify practice2

--- leetcode/src/main/java/slidingwindow/_81_sliding_window_maximum_239/Practice2.java
+++ leetcode/src/main/java/slidingwindow/_81_sliding_window_maximum_239/Practice2.java
@@ -1,7 +1,21 @@
 package slidingwindow._81_sliding_window_maximum_239;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Practice2 {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        for(int i=0; i+k-1<nums.length; i++){
+            int max =Integer.MIN_VALUE;
+            for(int j=i; j<i+k; j++){
+                if(max<nums[j]){
+                    max = nums[j];
+                }
+            }
+        }
 
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
+    // error
 }
