886278258ce8910377206e6ca1437f09dce21328
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-09-30 20:51:33 +0900

    feat(134): create Practice2

---
+++ leetcode/src/main/java/greedy/_87_gas_station_134/Practice2.java
@@ -0,0 +1,26 @@
+package greedy._87_gas_station_134;
+
+public class Practice2 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        for(int i=0; i< gas.length; i++){
+            int count =0;
+            int sum=gas[i];
+            int index = i;
+            while(count < gas.length){
+                if(sum-cost[(index)%gas.length]>=0){
+                    sum =sum+ gas[(index+1)%gas.length]-cost[(index)%gas.length];
+                } else{
+                    break;
+                }
+                count++;
+                index++;
+            }
+            if(count == gas.length){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Time Limit Exceeded
+}
