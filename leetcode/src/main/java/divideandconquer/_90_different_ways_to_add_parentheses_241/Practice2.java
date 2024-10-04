8d19dc4a21cf89b625a5cf8f5bd1f34c94365fc5
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-10-04 10:42:41 +0900

    feat(241): modify Practice2

--- leetcode/src/main/java/divideandconquer/_90_different_ways_to_add_parentheses_241/Practice2.java
+++ leetcode/src/main/java/divideandconquer/_90_different_ways_to_add_parentheses_241/Practice2.java
@@ -1,17 +1,24 @@
 package divideandconquer._90_different_ways_to_add_parentheses_241;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Practice2 {
+    Map<String, List<Integer>> cache = new HashMap<>();
     public List<Integer> diffWaysToCompute(String expression) {
         List<Integer> result = new ArrayList<>();
+
+        if(cache.containsKey(expression)){
+            return cache.get(expression);
+        }
 
         for (int i = 0; i < expression.length(); i++) {
             if (expression.charAt(i) == '-' || expression.charAt(i) == '+'
                     || expression.charAt(i) == '*' || expression.charAt(i) == '/') {
                 List<Integer> left = diffWaysToCompute(expression.substring(0, i));
-                List<Integer> right = diffWaysToCompute(expression.substring(i+1));
+                List<Integer> right = diffWaysToCompute(expression.substring(i + 1));
 
                 for (int l : left) {
                     for (int r : right) {
@@ -32,12 +39,12 @@ public class Practice2 {
             }
         }
 
-        if(result.isEmpty()){
+        if (result.isEmpty()) {
             result.add(Integer.parseInt(expression));
         }
 
+        cache.put(expression,result);
         return result;
     }
-
-    // 3ms
+    // 3ms -> 2ms
 }
