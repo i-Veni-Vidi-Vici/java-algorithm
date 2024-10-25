a1b4b79d5ee362eb54ec3d5f2dd41dfde94c0233
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-10-25 12:03:23 +0900

    feat(100): create Practice1

---
+++ leetcode/src/main/java/dfs/same_tree_100/Practice1.java
@@ -0,0 +1,41 @@
+package dfs.same_tree_100;
+
+public class Practice1 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null) {
+            return false;
+        }
+        if (q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    // 0ms
+}
