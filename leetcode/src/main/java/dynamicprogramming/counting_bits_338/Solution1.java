4646cfb6c45f549a99271b4442eaf6dfd967afc9
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-12-14 15:01:28 +0900

    feat(338): add Solution1

---
+++ leetcode/src/main/java/dynamicprogramming/counting_bits_338/Solution1.java
@@ -0,0 +1,12 @@
+package dynamicprogramming.counting_bits_338;
+
+public class Solution1 {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+    // 2ms
+}
