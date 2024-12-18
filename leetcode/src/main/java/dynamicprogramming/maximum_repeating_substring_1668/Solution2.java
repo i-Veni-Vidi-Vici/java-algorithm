eb39a432685e35fbc0b782f3042a19a59f830d86
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-12-18 19:36:08 +0900

    feat(1668): add Solution2

---
+++ leetcode/src/main/java/dynamicprogramming/maximum_repeating_substring_1668/Solution2.java
@@ -0,0 +1,20 @@
+package dynamicprogramming.maximum_repeating_substring_1668;
+
+public class Solution2 {
+    public int maxRepeating(String sequence, String word) {
+        int n = sequence.length();
+        int m = word.length();
+        int[] dp = new int[n + 1];
+        int maxRepeat = 0;
+
+        for (int i = m; i <= n; i++) {
+            if (sequence.substring(i - m, i).equals(word)) {
+                dp[i] = dp[i - m] + 1;
+                maxRepeat = Math.max(maxRepeat, dp[i]);
+            }
+        }
+
+        return maxRepeat;
+    }
+    // 0ms
+}
