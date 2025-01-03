86986fea303a78a334dd84a1e27b4fbf8649bf67
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-01-03 19:24:24 +0900

    feat(131): add Solution1

---
+++ leetcode/src/main/java/dynamicprogramming/_131/Solution1.java
@@ -0,0 +1,39 @@
+package dynamicprogramming._131;
+
+import java.util.*;
+public class Solution1 {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(String s, int start, List<String> current, List<List<String>> result) {
+        if (start >= s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                current.add(s.substring(start, end + 1));
+                backtrack(s, end + 1, current, result);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    // 주어진 범위의 문자열이 회문인지 확인하는 함수
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    // 10ms
+}
