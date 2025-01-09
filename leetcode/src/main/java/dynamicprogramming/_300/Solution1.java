a36a9eb66c0148580b9fe97a9e1db95ddbd62cc1
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-01-10 08:59:58 +0900

    feat(300): add Solution1

---
+++ leetcode/src/main/java/dynamicprogramming/_300/Solution1.java
@@ -0,0 +1,28 @@
+package dynamicprogramming._300;
+
+import java.util.Arrays;
+
+public class Solution1 {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int maxLength = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+
+        return maxLength;
+    }
+
+    // 40ms
+}
