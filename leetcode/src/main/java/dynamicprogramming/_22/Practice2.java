71bb9a7faee9687ae772182ed6d816711b35bf8c
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-02-12 11:59:19 +0900

    feat(22): create Practice2

--- leetcode/src/main/java/dynamicprogramming/_22/Practice2.java
+++ leetcode/src/main/java/dynamicprogramming/_22/Practice2.java
@@ -1,2 +1,30 @@
-package dynamicprogramming._22;public class Practice2 {
+package dynamicprogramming._22;
+
+import java.util.*;
+
+public class Practice2 {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        dfs(n, 0, 0, result, "");
+        return result;
+    }
+
+    private void dfs(int n, int open, int close, List<String> result, String current){
+        if(open<close){
+            return;
+        }
+        if(n*2 == open + close){
+            result.add(current);
+            return;
+        }
+
+        if(open<n){
+            dfs(n, open+1, close, result, current+"(");
+        }
+
+        if(close<n){
+            dfs(n, open, close+1, result, current+")");
+        }
+    }
+    // 2ms
 }
