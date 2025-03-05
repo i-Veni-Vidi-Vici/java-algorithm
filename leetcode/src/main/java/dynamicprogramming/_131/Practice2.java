d068790e251d83e91de83bf82fdf299c42fd79d6
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-03-05 14:43:40 +0900

    feat(131): modify Practice2

--- leetcode/src/main/java/dynamicprogramming/_131/Practice2.java
+++ leetcode/src/main/java/dynamicprogramming/_131/Practice2.java
@@ -1,5 +1,7 @@
 package dynamicprogramming._131;
 
+import java.util.*;
+
 public class Practice2 {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
@@ -8,28 +10,28 @@ public class Practice2 {
         return result;
     }
 
-    private void dfs(List<List<String>> result, String s,List<String> current, int start ){
-        if(start== s.length()){
+    private void dfs(List<List<String>> result, String s, List<String> current, int start) {
+        if (start == s.length()) {
             result.add(new ArrayList<>(current));
             return;
         }
 
-        for(int i=start; i<s.length(); i++){
-            if(isPalindrome(s.substring(start, i+1))){
-                current.add(s.substring(start, i+1));
-                dfs(result, s, current, i+1);
-                current.remove(current.size()-1);
+        for (int i = start; i < s.length(); i++) {
+            if (isPalindrome(s.substring(start, i + 1))) {
+                current.add(s.substring(start, i + 1));
+                dfs(result, s, current, i + 1);
+                current.remove(current.size() - 1);
             }
         }
 
     }
 
-    private boolean isPalindrome(String s){
+    private boolean isPalindrome(String s) {
         int len = s.length();
-        int start=0;
-        int cur = len/2;
-        while(cur>0){
-            if(s.charAt(start)!= s.charAt(len-1)){
+        int start = 0;
+        int cur = len / 2;
+        while (cur > 0) {
+            if (s.charAt(start) != s.charAt(len - 1)) {
                 return false;
             }
             start++;
