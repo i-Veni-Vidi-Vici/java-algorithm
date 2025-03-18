61bacbddb9b834a9595f43778f46a9f9ad0fa7b8
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-03-18 13:12:17 +0900

    feat(16): create Practice1

---
+++ leetcode/src/main/java/linear/array/_16/Practice1.java
@@ -0,0 +1,27 @@
+package linear.array._16;
+
+public class Practice1 {
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            int left = i+1;
+            int right = nums.length-1;
+
+            while(left<right){
+
+                if(Math.abs(target- (nums[i]+nums[left]+nums[right]))<Math.abs(target-  min)){
+                    min =nums[i]+nums[left]+nums[right];
+
+                }
+                left++;
+                right--;
+            }
+        }
+
+        return min;
+    }
+    // leetcode error
+}
