0cb08af609d3475123b308321a3d14b2be062fc9
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-03-21 11:39:21 +0900

    faet(309): create Practice2

---
+++ leetcode/src/main/java/dynamicprogramming/_309/Practice2.java
@@ -0,0 +1,28 @@
+package dynamicprogramming._309;
+
+public class Practice2 {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int n = prices.length;
+
+        int[] hold = new int[n];
+        int[] sold = new int[n];
+        int[] rest = new int[n];
+
+        hold[0] = -prices[0];
+        sold[0] = 0;
+        rest[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            hold[i] = Math.max(hold[i-1], rest[i-1] - prices[i]);
+            sold[i] = hold[i-1] + prices[i];
+            rest[i] = Math.max(sold[i-1], rest[i-1]);
+        }
+
+        return Math.max(sold[n-1], rest[n-1]);
+    }
+    //
+}
