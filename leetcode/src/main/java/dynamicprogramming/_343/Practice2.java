02a7e99916dc1985e6847a179d2410b738cf8256
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-03-29 15:13:15 +0900

    feat(33): create Practice3

--- leetcode/src/main/java/dynamicprogramming/_343/Practice2.java
+++ leetcode/src/main/java/dynamicprogramming/_343/Practice2.java
@@ -1,2 +1,24 @@
-package dynamicprogramming._343;public class Practice2 {
+package dynamicprogramming._343;
+
+public class Practice2 {
+    public int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 2; j <= i/2; j++) {
+                int mul = dp[j] * dp[i-j];
+                dp[i] = Math.max(dp[i], mul);
+            }
+        }
+
+        return dp[n];
+    }
+    // 1ms
 }
