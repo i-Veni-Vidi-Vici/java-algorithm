1483978a388793c876a9ab006982ab1d6a325884
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-04-02 17:31:04 +0900

    feat(368): create Practice2

---
+++ leetcode/src/main/java/dynamicprogramming/_368/Practice2.java
@@ -0,0 +1,40 @@
+package dynamicprogramming._368;
+
+import java.util.*;
+public class Practice2 {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return new ArrayList<>();
+
+        Arrays.sort(nums);
+        int[] dp = new int[n];
+        int[] prev = new int[n];
+        Arrays.fill(dp, 1);
+        Arrays.fill(prev, -1);
+
+        int maxIndex = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > dp[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int i = maxIndex;
+        while (i >= 0) {
+            result.add(nums[i]);
+            i = prev[i];
+        }
+
+
+        return result;
+    }
+    // 13ms
+}
