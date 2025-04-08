52fd96360d50e0d21b241d627f54aa94ecf76485
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-04-08 23:59:20 +0900

    feat(보호소): create Practice

---
+++ programmers/src/main/java/sql/보호소/Practice.sql
@@ -0,0 +1,4 @@
+SELECT i.ANIMAL_ID, i.ANIMAL_TYPE, i.name
+from ANIMAL_INS i inner join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
+where i.SEX_UPON_INTAKE != o.SEX_UPON_OUTCOME
+order by i.ANIMAL_ID
