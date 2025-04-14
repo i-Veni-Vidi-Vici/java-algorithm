0637d88bbbdad2997436d99da43abe3d666dee70
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-04-14 15:10:16 +0900

    feat(377): create Practice2_1

---
+++ leetcode/src/main/java/dynamicprogramming/_377/Practice2_1.java
@@ -0,0 +1,18 @@
+package dynamicprogramming._377;
+
+public class Practice2_1 {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 0; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i >= nums[j]) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+
+        return dp[target];
+    }
+    // 3ms
+}
