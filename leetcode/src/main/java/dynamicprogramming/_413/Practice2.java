1520c7cc48a02c00b58a452e14683a434ea9aa72
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-04-18 12:15:42 +0900

    feat(413): create Pracitce2

---
+++ leetcode/src/main/java/dynamicprogramming/_413/Practice2.java
@@ -0,0 +1,22 @@
+package dynamicprogramming._413;
+
+public class Practice2 {
+    public int numberOfArithmeticSlices(int[] nums) {
+        if (nums.length < 3) return 0;
+
+        int count = 0;
+        int current = 0;
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
+                current++;
+                count += current;
+            } else {
+                current = 0;
+            }
+        }
+
+        return count;
+    }
+    // 0ms
+}
