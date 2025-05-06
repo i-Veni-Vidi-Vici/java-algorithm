730622e3216cdfba9ed8a76dbc80794347afb965
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-05-06 12:36:56 +0900

    feat(516): create Practice1

---
+++ leetcode/src/main/java/dynamicprogramming/_516/Practice1.java
@@ -0,0 +1,27 @@
+package dynamicprogramming._516;
+
+public class Practice1 {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = (len == 2) ? 2 : dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+    // 38ms
+}
