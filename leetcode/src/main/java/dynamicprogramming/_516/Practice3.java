be157e38c6f4c2df560f35c0d2df26feae1dd7ce
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-05-08 22:42:07 +0900

    feat(516): create Practice3

---
+++ leetcode/src/main/java/dynamicprogramming/_516/Practice3.java
@@ -0,0 +1,27 @@
+package dynamicprogramming._516;
+
+public class Practice3 {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                // len == 2 필요 없음
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+    // 34ms
+}
