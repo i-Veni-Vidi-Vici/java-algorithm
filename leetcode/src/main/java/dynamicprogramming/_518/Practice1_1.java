6f7210b160451c7bd7786d3ab26d08ff87ffe610
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-05-11 13:08:49 +0900

    feat(518): create Practice1

---
+++ leetcode/src/main/java/dynamicprogramming/_518/Practice1_1.java
@@ -0,0 +1,17 @@
+package dynamicprogramming._518;
+
+public class Practice1_1 {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for(int coin : coins){
+            for(int i=coin; i<=amount; i++){
+                dp[i] += dp[i-coin];
+            }
+        }
+
+        return dp[amount];
+    }
+
+    // 4ms
+}
