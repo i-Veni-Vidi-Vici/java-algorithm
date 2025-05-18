9b51536e5bcde7d26f05dadf8928f844a9514f60
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2025-05-18 11:20:27 +0900

    feat(1043): create Practice2

---
+++ leetcode/src/main/java/dynamicprogramming/_1043/Practice2.java
@@ -0,0 +1,20 @@
+package dynamicprogramming._1043;
+
+public class Practice2 {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int maxInPartition = 0;
+            for (int j = 1; j <= k && i - j >= 0; j++) {
+                maxInPartition = Math.max(maxInPartition, arr[i - j]);
+                dp[i] = Math.max(dp[i], dp[i - j] + maxInPartition * j);
+            }
+        }
+
+        return dp[n];
+    }
+
+    // 5ms
+}
